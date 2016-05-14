7a1b915351c27c6f7e989ebbc96b76ec7424ae55
Noam Reuveni <nicenoam@gmail.com>
2016-05-14 11:06:16 -0400

    Update gulp file for concat and inline tasks

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+// ## Globals
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var inlineCss = require('gulp-inline-css');
+var runSequence  = require('run-sequence');
+
+
+//Compiled all in modules
+gulp.task('compile', function() {
+  return gulp.src(['./Build/*.html'])
+    .pipe(concat('compiled.html'))
+    .pipe(gulp.dest('./Compiled'));
+});
+
+
+gulp.task('inline', function() {
+    return gulp.src('./Compiled/compiled.html')
+        .pipe(inlineCss({
+	        	applyStyleTags: true,
+	        	applyLinkTags: true,
+	        	removeStyleTags: false,
+	        	removeLinkTags: false,
+	        	preserveMediaQueries: true
+        }))
+        .pipe(gulp.dest('./Output'));
+});
+
+// Run Build
+gulp.task('build', function (callback) {
+  runSequence('compile',
+              'inline',
+              callback);
+});
+
+// Watch
+gulp.task('watch', function () {
+   gulp.watch(['./assets/css/less/*.less','./assets/js/js/*.js'], ['compile']);
+});
+
